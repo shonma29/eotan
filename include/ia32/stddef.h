506730497aa082588916845e440abbf10bed5746
rick <rick@rene.noriko.or.tp>
2012-06-29 22:40:29 +0900

    Add headers for stand alone environment.

---
+++ include/ia32/stddef.h
@@ -0,0 +1,38 @@
+#ifndef _STDDEF_H_
+#define _STDDEF_H_
+/*
+This is free and unencumbered software released into the public domain.
+
+Anyone is free to copy, modify, publish, use, compile, sell, or
+distribute this software, either in source code form or as a compiled
+binary, for any purpose, commercial or non-commercial, and by any
+means.
+
+In jurisdictions that recognize copyright laws, the author or authors
+of this software dedicate any and all copyright interest in the
+software to the public domain. We make this dedication for the benefit
+of the public at large and to the detriment of our heirs and
+successors. We intend this dedication to be an overt act of
+relinquishment in perpetuity of all present and future rights to this
+software under copyright law.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+OTHER DEALINGS IN THE SOFTWARE.
+
+For more information, please refer to <http://unlicense.org/>
+*/
+
+typedef int ptrdiff_t;
+typedef unsigned int size_t;
+typedef unsigned int wchar_t;
+
+#define NULL ((void*)0)
+
+#define offsetof(type, member) (size_t)&(((type*)0)->member)
+
+#endif
---
+++ include/stddef.h
@@ -0,0 +1,1 @@
+ia32/stddef.h
