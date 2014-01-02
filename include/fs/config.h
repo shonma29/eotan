40279c540e67433544b9794ec5e2490a7d91ce84
rick <rick@rene.noriko.or.tp>
2014-01-02 16:09:06 +0900

    Delete unused definitions.

--- include/fs/config.h
+++ include/fs/config.h
@@ -24,13 +24,11 @@ Version 2, June 1991
 
 /* 各種制限値 */
 #define MAX_BODY_SIZE	1024
-#define MAX_LINE_LENGTH	512
 #define MAXFS		1
 #define MAX_MOUNT	5
 #define MAX_INODE	200
 #define MAX_OPEN	100
 #define MAX_NAMELEN	255
-#define MAX_SIGNAL	32
 #define MAX_PROCESS	100
 
 #define PROC_NAME_LEN	35
