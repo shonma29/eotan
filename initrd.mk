9418dcb772edc8f672a4142030824fca4fc39652
rick <rick@rene.noriko.or.tp>
2014-03-06 00:01:58 +0900

    Delete useless entries.

--- initrd.mk
+++ initrd.mk
@@ -10,17 +10,6 @@ all: clean
 	${MKFS}/statfs ${TARGET} mkdir /dev
 	${MKFS}/statfs ${TARGET} mknod /dev/console	0x00000000
 	${MKFS}/statfs ${TARGET} mknod /dev/keyboard	0x00010000
-	${MKFS}/statfs ${TARGET} mknod /dev/psaux	0x00020000
-	${MKFS}/statfs ${TARGET} mknod /dev/ide0	0x80010000
-	${MKFS}/statfs ${TARGET} mknod /dev/ide1	0x80010001
-	${MKFS}/statfs ${TARGET} mknod /dev/ide2	0x80010002
-	${MKFS}/statfs ${TARGET} mknod /dev/ide3	0x80010003
-	${MKFS}/statfs ${TARGET} mknod /dev/ide4	0x80010004
-	${MKFS}/statfs ${TARGET} mknod /dev/ide5	0x80010005
-	${MKFS}/statfs ${TARGET} mknod /dev/ide6	0x80010006
-	${MKFS}/statfs ${TARGET} mknod /dev/ide7	0x80010007
-	${MKFS}/statfs ${TARGET} mknod /dev/ide8	0x80010008
-	${MKFS}/statfs ${TARGET} mknod /dev/ide9	0x80010009
 	${MKFS}/statfs ${TARGET} mknod /dev/rd	0x80020000
 	${MKFS}/statfs ${TARGET} dir /dev
 	${MKFS}/statfs ${TARGET} write /initrd.mk initrd.mk
