f1837e7bb24b3b17cd2cb3a8df9a83f95b33a749
rick <rick@rene.noriko.or.tp>
2015-02-11 22:28:05 +0900

    Arrange code.

--- servers/fs/cache.c
+++ servers/fs/cache.c
@@ -25,19 +25,14 @@ Version 2, June 1991
 
 #undef USE_MALLOC
 
-#define HASH_SIZE	30
-#ifdef USE_MALLOC
-//static int CACHE_SIZE = 2048;
-static int CACHE_SIZE = 256;
-#else
+#define HASH_SIZE	31
 #define CACHE_SIZE  (200)
-#endif
 
 typedef struct {
     short int dirty;
     short int lru_next, lru_prev;
     short int hash_next, hash_prev;
-    W fd;
+    W device_id;
     W blockno;
     B buf[CACHE_BLOCK_SIZE];
 } block_cache_t;
@@ -50,10 +45,17 @@ static block_cache_t cache_data[CACHE_SIZE];
 static short int cache_head;
 static short int hash_table[HASH_SIZE + 1];
 
+static void lru_append(const short int cache_no, block_cache_t *cp);
+static void lru_remove(const short int cache_no, block_cache_t *cp);
+static short int hash_calc(const W blockno);
+static void hash_add(const short int key, block_cache_t *cp);
+static void hash_remove(const short int key, block_cache_t *cp);
+static short int hash_find(const short int key, const W device_id, const W blockno);
+
 
 void purge_cache(void)
 {
-    int i;
+    short int i;
 
     for (i = 0; i < CACHE_SIZE; ++i) {
 	memset((B*)(&cache_data[i]), 0, sizeof(block_cache_t));
@@ -75,10 +77,6 @@ void purge_cache(void)
 void init_cache(void)
 {
 #ifdef USE_MALLOC
-    CACHE_SIZE = pageRoundUp(CACHE_SIZE * sizeof(block_cache_t))
-	- 12;
-    /* 12 は ITRON/kernlib/malloc.c の alloc_entry_t から来ている */
-    CACHE_SIZE /= sizeof(block_cache_t);
     cache_data =
 	(block_cache_t *) malloc(sizeof(block_cache_t) * (CACHE_SIZE));
 #endif
@@ -86,9 +84,9 @@ void init_cache(void)
     purge_cache();
 }
 
-void get_cache(W fd, W blockno, W * cn, B ** ptr)
+void get_cache(W device_id, W blockno, W * cn, B ** ptr)
 {
-    int i, hn;
+    short int i, hn;
     block_cache_t *cp;
 
     if (blockno < 0) {
@@ -96,11 +94,8 @@ void get_cache(W fd, W blockno, W * cn, B ** ptr)
 	*ptr = NULL;
 	return;
     }
-    hn = (blockno % HASH_SIZE) + 1;
-    for (i = hash_table[hn]; i >= 0; i = cache_data[i].hash_next) {
-	if ((cache_data[i].fd == fd) && (cache_data[i].blockno == blockno))
-	    break;
-    }
+    hn = hash_calc(blockno);
+    i = hash_find(hn, device_id, blockno);
     if (i < 0) {
 	*cn = cache_head;
 	if (cache_head == -1) {
@@ -114,7 +109,7 @@ void get_cache(W fd, W blockno, W * cn, B ** ptr)
 	if (cp->dirty) {
 	    W rsize;
 
-	    if (write_device(cp->fd, cp->buf, cp->blockno * CACHE_BLOCK_SIZE,
+	    if (write_device(cp->device_id, cp->buf, cp->blockno * CACHE_BLOCK_SIZE,
 		    CACHE_BLOCK_SIZE, &rsize)) {
 		dbg_printf("fs: get_cache write error\n");
 		*cn = -1;
@@ -127,27 +122,17 @@ void get_cache(W fd, W blockno, W * cn, B ** ptr)
 	/* remove from hash chain */
 	if (cp->blockno >= 0) {
 	    /* 既に hash に登録されている場合 */
-	    if (cp->hash_prev == -1) {
-		/* head of hash chain */
-		hash_table[(cp->blockno % HASH_SIZE) + 1] = cp->hash_next;
-	    } else {
-		cache_data[cp->hash_prev].hash_next = cp->hash_next;
-	    }
-
-	    if (cp->hash_next != -1) {
-		/* not tail of hash chain */
-		cache_data[cp->hash_next].hash_prev = cp->hash_prev;
-	    }
+	    hash_remove(hash_calc(cp->blockno), cp);
 	}
 	memset(cp->buf, 0, CACHE_BLOCK_SIZE);
 
 	{
 		W rsize;
 
-		if (read_device(fd, cp->buf, blockno * CACHE_BLOCK_SIZE, CACHE_BLOCK_SIZE,
-			&rsize)) {
+		if (read_device(device_id, cp->buf, blockno * CACHE_BLOCK_SIZE,
+			CACHE_BLOCK_SIZE, &rsize)) {
 		    dbg_printf("fs: get_cache read error\n");
-		    cp->fd = 0;
+		    cp->device_id = 0;
 		    cp->blockno = -1;
 		    *cn = -1;
 		    *ptr = NULL;
@@ -155,78 +140,43 @@ void get_cache(W fd, W blockno, W * cn, B ** ptr)
 		}
 	}
 
-	cp->fd = fd;
+	cp->device_id = device_id;
 	cp->blockno = blockno;
 	*ptr = cp->buf;
 
 	/* add to hash chain */
-	cp->hash_next = hash_table[hn];
-	cp->hash_prev = -1;
-	if (hash_table[hn] != -1)
-	    cache_data[hash_table[hn]].hash_prev = cache_head;
-	hash_table[hn] = cache_head;
+	hash_add(hn, cp);
 
 	/* remove from lru chain */
-	if (cache_head == cp->lru_next) {
-	    cache_head = -1;
-	} else {
-	    cache_head = cp->lru_next;
-	}
-	cache_data[cp->lru_next].lru_prev = cp->lru_prev;
-	cache_data[cp->lru_prev].lru_next = cp->lru_next;
-	cp->lru_prev = (-1);
-	cp->lru_next = (-1);
+	lru_remove(cache_head, cp);
     } else {
 	*cn = i;
 	*ptr = cache_data[i].buf;
 	cp = &cache_data[i];
 	if (cp->lru_next != -1) {
-	    if (cache_head == i) {
-		if (cache_head == cp->lru_next) {
-		    cache_head = (-1);
-		} else {
-		    cache_head = cp->lru_next;
-		}
-	    }
-	    cache_data[cp->lru_next].lru_prev = cp->lru_prev;
-	    cache_data[cp->lru_prev].lru_next = cp->lru_next;
-	    cp->lru_next = (-1);
-	    cp->lru_prev = (-1);
+	    lru_remove(i, cp);
 	}
     }
 }
 
-void check_cache(W fd, W blockno, W * cn)
+void check_cache(W device_id, W blockno, W * cn)
 {
-    int i, hn;
+    short int i;
     block_cache_t *cp;
 
     if (blockno < 0) {
 	*cn = -1;
 	return;
     }
-    hn = (blockno % HASH_SIZE) + 1;
-    for (i = hash_table[hn]; i >= 0; i = cache_data[i].hash_next) {
-	if ((cache_data[i].fd == fd) && (cache_data[i].blockno == blockno))
-	    break;
-    }
+
+    i = hash_find(hash_calc(blockno), device_id, blockno);
     if (i < 0) {
 	*cn = -1;
     } else {
 	*cn = i;
 	cp = &cache_data[i];
 	if (cp->lru_next != -1) {
-	    if (cache_head == i) {
-		if (cache_head == cp->lru_next) {
-		    cache_head = (-1);
-		} else {
-		    cache_head = cp->lru_next;
-		}
-	    }
-	    cache_data[cp->lru_next].lru_prev = cp->lru_prev;
-	    cache_data[cp->lru_prev].lru_next = cp->lru_next;
-	    cp->lru_next = (-1);
-	    cp->lru_prev = (-1);
+	    lru_remove(i, cp);
 	}
     }
 }
@@ -255,33 +205,24 @@ void put_cache(W cn, W dirty)
 	dbg_printf("fs: block already back!! %d next %d\n", cn,
 	       cp->lru_next);
     } else {
-	if (cache_head == -1) {
-	    cache_head = cn;
-	    cp->lru_prev = cn;
-	    cp->lru_next = cn;
-	} else {
-	    cp->lru_prev = cache_data[cache_head].lru_prev;
-	    cp->lru_next = cache_head;
-	    cache_data[cp->lru_prev].lru_next = cn;
-	    cache_data[cache_head].lru_prev = cn;
-	}
+    	lru_append(cn, cp);
     }
 }
 
-W sync_cache(W fd, W umflag)
+W sync_cache(W device_id, W umflag)
 {
-    int i;
+    short int i;
     block_cache_t *cp;
     W error_no;
 
     for (i = 0; i < CACHE_SIZE; ++i) {
 	cp = &cache_data[i];
-	if (cp->fd == fd) {
+	if (cp->device_id == device_id) {
 	    if (cp->dirty) {
 		W rsize;
 
 		error_no =
-		    write_device(fd, cp->buf, cp->blockno * CACHE_BLOCK_SIZE,
+		    write_device(device_id, cp->buf, cp->blockno * CACHE_BLOCK_SIZE,
 			    CACHE_BLOCK_SIZE, &rsize);
 		if (error_no) {
 		    dbg_printf("fs: sync_cache write error\n");
@@ -290,9 +231,79 @@ W sync_cache(W fd, W umflag)
 		cp->dirty = 0;
 	    }
 	    if (umflag) {
-		cp->fd = 0;
+		cp->device_id = 0;
 	    }
 	}
     }
     return (EOK);
+}
+
+static void lru_append(const short int cache_no, block_cache_t *cp)
+{
+    if (cache_head == -1) {
+	cache_head = cache_no;
+	cp->lru_prev = cache_no;
+	cp->lru_next = cache_no;
+    } else {
+	cp->lru_prev = cache_data[cache_head].lru_prev;
+	cp->lru_next = cache_head;
+	cache_data[cp->lru_prev].lru_next = cache_no;
+	cache_data[cache_head].lru_prev = cache_no;
+    }
+}
+
+static void lru_remove(const short int cache_no, block_cache_t *cp)
+{
+    if (cache_head == cache_no) {
+	if (cache_head == cp->lru_next) {
+	    cache_head = (-1);
+	} else {
+	    cache_head = cp->lru_next;
+	}
+    }
+    cache_data[cp->lru_next].lru_prev = cp->lru_prev;
+    cache_data[cp->lru_prev].lru_next = cp->lru_next;
+    cp->lru_next = (-1);
+    cp->lru_prev = (-1);
+}
+
+static short int hash_calc(const W blockno)
+{
+    return (blockno % HASH_SIZE) + 1;
+}
+
+static void hash_add(const short int key, block_cache_t *cp)
+{
+    cp->hash_next = hash_table[key];
+    cp->hash_prev = -1;
+    if (hash_table[key] != -1)
+	cache_data[hash_table[key]].hash_prev = cache_head;
+    hash_table[key] = cache_head;
+}
+
+static void hash_remove(const short int key, block_cache_t *cp)
+{
+    if (cp->hash_prev == -1) {
+	/* head of hash chain */
+	hash_table[key] = cp->hash_next;
+    } else {
+	cache_data[cp->hash_prev].hash_next = cp->hash_next;
+    }
+
+    if (cp->hash_next != -1) {
+	/* not tail of hash chain */
+	cache_data[cp->hash_next].hash_prev = cp->hash_prev;
+    }
+}
+
+static short int hash_find(const short int key, const W device_id, const W blockno)
+{
+    int i;
+
+    for (i = hash_table[key]; i >= 0; i = cache_data[i].hash_next) {
+	if ((cache_data[i].device_id == device_id) && (cache_data[i].blockno == blockno))
+	    break;
+    }
+
+    return i;
 }
