ed811ee14f9f53b3c0e5eea56db4cd627b881c0b
rick <rick@rene.noriko.or.tp>
2017-06-11 22:22:41 +0900

    Show more information in build stage.

--- starter/pcat/append.c
+++ starter/pcat/append.c
@@ -24,18 +24,23 @@ OTHER DEALINGS IN THE SOFTWARE.
 
 For more information, please refer to <http://unlicense.org/>
 */
+#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/stat.h>
-#include <errno.h>
+#include "../../include/elf.h"
+#include "../../include/boot/initrd.h"
 #include "../../include/boot/modules.h"
 
 #define ERR_OK (0)
 #define ERR_ARG (1)
 #define ERR_FILE (2)
+#define ERR_DATA (3)
 
-#define HELP "usage:\n\tappend moduleType filename blockSize\n"
+#define HELP "usage:\n\tappend moduleType filename alignment [id]\nServers need their id.\n"
+#define BAD_ELF "bad ELF\n"
 
+static int show_elf_info(FILE *out, const int moduleType, const char *fileName);
 static int append(FILE *out, const int moduleType, const char *fileName,
 		const int blkSize, const int arg);
 static int dup(FILE *out, size_t size, FILE *in);
@@ -46,6 +51,7 @@ static int appendEnd(FILE *out);
 int main(int argc, char **argv)
 {
 	int type;
+	int result;
 
 	switch (argc) {
 	case 2:
@@ -55,15 +61,35 @@ int main(int argc, char **argv)
 
 		break;
 
-	case 5:
+	case 4:
 		type = atoi(argv[1]);
 
 		switch (type) {
 		case mod_kernel:
 		case mod_driver:
-		case mod_server:
 		case mod_user:
+			result = show_elf_info(stderr, type, argv[2]);
+			if (result)
+				return result;
+
 		case mod_initrd:
+			return append(stdout, type, argv[2], atoi(argv[3]), 0);
+
+		default:
+			break;
+		}
+
+		break;
+
+	case 5:
+		type = atoi(argv[1]);
+
+		switch (type) {
+		case mod_server:
+			result = show_elf_info(stderr, type, argv[2]);
+			if (result)
+				return result;
+
 			return append(stdout, type, argv[2], atoi(argv[3]),
 					atoi(argv[4]));
 
@@ -81,8 +107,81 @@ int main(int argc, char **argv)
 	return ERR_ARG;
 }
 
+static int show_elf_info(FILE *out, const int moduleType, const char *fileName)
+{
+	int ret = ERR_OK;
+	FILE *fp = NULL;
+
+	do {
+		Elf32_Ehdr eHdr;
+		Elf32_Shdr sHdr;
+		int i;
+		void *first = (void*)(-1);
+		void *last = 0;
+		size_t len = 0;
+
+		fp = fopen(fileName, "rb");
+		if (!fp) {
+			fprintf(stderr, "open error(%d)\n", errno);
+			ret = ERR_FILE;
+			break;
+		}
+
+		if (fread(&eHdr, 1, sizeof(eHdr), fp) != sizeof(eHdr)) {
+			fprintf(stderr, "read error(%d)\n", errno);
+			ret = ERR_FILE;
+			break;
+		}
+
+		if (!isValidModule(&eHdr)) {
+			fprintf(stderr, BAD_ELF);
+			ret = ERR_DATA;
+			break;
+		}
+
+		if (fseek(fp, eHdr.e_shoff, SEEK_SET)) {
+			fprintf(stderr, BAD_ELF);
+			ret = ERR_DATA;
+			break;
+		}
+
+		for (i = 0; i < eHdr.e_shnum; i++) {
+			if (fread(&sHdr, 1, sizeof(sHdr), fp) != sizeof(sHdr)) {
+				fprintf(stderr, BAD_ELF);
+				ret = ERR_DATA;
+				break;
+			}
+
+			if (sHdr.sh_addr)
+				if ((size_t)(sHdr.sh_addr) < (size_t)first)
+					first = (void*)(sHdr.sh_addr);
+
+			if ((size_t)(sHdr.sh_addr) > (size_t)last) {
+				last = (void*)(sHdr.sh_addr);
+				len = 0;
+			}
+
+			if ((size_t)(sHdr.sh_addr) == (size_t)last)
+				if (sHdr.sh_size > len)
+					len = sHdr.sh_size;
+		}
+
+		fprintf(out, "%p: %p\n",
+				first, (void*)((size_t)last + len - 1));
+
+	} while (0);
+
+	if (fp) {
+		if (fclose(fp)) {
+			fprintf(stderr, "close error(%d)\n", errno);
+		}
+	}
+
+	return ret;
+}
+
 static int append(FILE *out, const int moduleType, const char *fileName,
-		const int blkSize, const int arg)
+		const int alingment, const int arg)
 {
 	int ret = ERR_OK;
 	FILE *fp = NULL;
@@ -99,8 +198,11 @@ static int append(FILE *out, const int moduleType, const char *fileName,
 			break;
 		}
 
+		if (moduleType == mod_initrd)
+			fprintf(stderr, "size %x\n", INITRD_SIZE);
+
 		h.type = moduleType;
-		h.length = (buf.st_size + (blkSize - 1)) & ~(blkSize - 1);
+		h.length = (buf.st_size + (alingment - 1)) & ~(alingment - 1);
 		h.bytes = buf.st_size;
 		h.zBytes = 0;
 		h.arg = arg;
