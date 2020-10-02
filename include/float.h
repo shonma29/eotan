9362c846dcd6bacb98050c1147a850fd56c3917a
rick <rick@rene.noriko.or.tp>
2020-10-02 19:23:53 +0900

    Add 'float.h'.

---
+++ include/float.h
@@ -0,0 +1,61 @@
+#ifndef _FLOAT_H_
+#define _FLOAT_H_
+/*
+This is free and unencumbered software released into the public domain.
+
+Anyone is free to copy, modify, publish, use, compile, sell, or
+distribute this software, either in source code form or as a compiled
+binary, for any purpose, commercial or non-commercial, and by any
+means.
+
+In jurisdictions that recognize copyright laws, the author or authors
+of this software dedicate any and all copyright interest in the
+software to the public domain. We make this dedication for the benefit
+of the public at large and to the detriment of our heirs and
+successors. We intend this dedication to be an overt act of
+relinquishment in perpetuity of all present and future rights to this
+software under copyright law.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+OTHER DEALINGS IN THE SOFTWARE.
+
+For more information, please refer to <http://unlicense.org/>
+*/
+
+#define FLT_RADIX __FLT_RADIX__
+
+//#define FLT_ROUNDS ()
+
+#define FLT_MANT_DIG __FLT_MANT_DIG__
+#define FLT_MAX_EXP __FLT_MAX_EXP__
+#define FLT_MIN_EXP __FLT_MIN_EXP__
+#define FLT_MAX_10_EXP __FLT_MAX_10_EXP__
+#define FLT_MIN_10_EXP __FLT_MIN_10_EXP__
+#define FLT_MAX __FLT_MAX__
+#define FLT_MIN __FLT_MIN__
+#define FLT_EPSILON __FLT_EPSILON__
+
+#define DBL_MANT_DIG __DBL_MANT_DIG__
+#define DBL_MAX_EXP __DBL_MAX_EXP__
+#define DBL_MIN_EXP __DBL_MIN_EXP__
+#define DBL_MAX_10_EXP __DBL_MAX_10_EXP__
+#define DBL_MIN_10_EXP __DBL_MIN_10_EXP__
+#define DBL_MAX __DBL_MAX__
+#define DBL_MIN __DBL_MIN__
+#define DBL_EPSILON __DBL_EPSILON__
+
+#define LDBL_MANT_DIG __LDBL_MANT_DIG__
+#define LDBL_MAX_EXP __LDBL_MAX_EXP__
+#define LDBL_MIN_EXP __LDBL_MIN_EXP__
+#define LDBL_MAX_10_EXP __LDBL_MAX_10_EXP__
+#define LDBL_MIN_10_EXP __LDBL_MIN_10_EXP__
+#define LDBL_MAX __LDBL_MAX__
+#define LDBL_MIN __LDBL_MIN__
+#define LDBL_EPSILON __LDBL_EPSILON__
+
+#endif
