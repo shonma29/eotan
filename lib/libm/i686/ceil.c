20b1f7383eb087c43ce05a9d8088a51e7ab3975c
rick <rick@rene.noriko.or.tp>
2025-03-30 18:09:13 +0900

    Add 'ceil'.

--- lib/libm/i686/ceil.c
+++ lib/libm/i686/ceil.c
@@ -1,5 +1,3 @@
-#ifndef _MATH_H_
-#define _MATH_H_
 /*
 This is free and unencumbered software released into the public domain.
 
@@ -26,45 +24,10 @@ OTHER DEALINGS IN THE SOFTWARE.
 
 For more information, please refer to <http://unlicense.org/>
 */
+#include <math.h>
 
-#define MATH_ERRNO (1)
-#define MATH_ERREXCEPT (2)
-#define math_errhandling (MATH_ERRNO)
 
-#define NAN (__builtin_nan(""))
-#define INFINITY (__builtin_inf())
-#define HUGE_VAL (__builtin_huge_val())
-
-#define M_PI (3.1415926535897932384626433832795028841971L)
-#define M_LN2 (0.6931471805599453094172321214581765680755L)
-#define M_LN10 (2.3025850929940456840179914546843642076011L)
-#define M_SQRT2 (1.4142135623730950488016887242096980785696L)
-
-extern int __isinf_double(const double);
-extern int __isnan_double(const double);
-
-#define isinf(x) _Generic((x), \
-	double: __isinf_double \
-)(x)
-#define isnan(x) _Generic((x), \
-	double: __isnan_double \
-)(x)
-
-extern double fabs(double);
-extern double frexp(double, int *);
-extern double ldexp(double, int);
-
-extern double exp(double);
-extern double log(double);
-extern double log10(double);
-extern double sqrt(double);
-
-extern double sin(double);
-extern double cos(double);
-extern double tan(double);
-extern double atan(double);
-
-extern double ceil(double);
-extern double floor(double);
-
-#endif
+double ceil(double x)
+{
+	return (-(floor(-x)));
+}
